2fa219fe82316fe265fd86c4ada6bb24aa60c037
Filipe <128107143+Filipecorr34@users.noreply.github.com>
2023-05-18 00:20:25 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "matrizes.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("=====Equipe=====\n");
+    printf("Filipe Correa da Silva\n");
+    printf("Paulo Vinicius de Freitas Rodrigues\n");
+    printf("Kelton de Jesus Freitas Cantao\n");
+
+    printf("\n");
+
+    void teste_todos();
+
+    return 0;
+}

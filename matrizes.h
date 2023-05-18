2d907fc879eb518d5445eee502de90b2e45114f9
Filipe <128107143+Filipecorr34@users.noreply.github.com>
2023-05-18 00:19:56 -0300

    Add files via upload

---
+++ matrizes.h
@@ -0,0 +1,18 @@
+#ifndef MATRIZES_H
+#define MATRIZES_H
+
+typedef struct {
+    float real;
+    float imag;
+} complex;
+
+void transposta(complex** a, complex** result, int l, int c);
+void conjugada(complex** a, complex** result, int l, int c);
+void hermitiana(complex** a, complex** result, int l, int c);
+void soma(complex** a, complex** b, complex** result, int l, int c);
+void subtracao(complex** a, complex** b, complex** result, int l, int c);
+void produto_escalar(complex* a, complex* b, complex* result, int tam);
+void produto_matricial(complex** a, complex** b, complex** result, int l, int c, int m);
+void teste_todos();
+
+#endif // MATRIZES_H
